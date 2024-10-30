d3d197626f8b7c25a8cdaec769ce911556d3c55a
Idan Bezalel <omerb1304@gmail.com>
2024-10-30 22:50:42 +0200

    Added new directories

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+from .base_module import BaseModule
+from .biomedclipseg import BiomedCLIPSeg
+from .clipseg import CLIPSeg
+from .cris_model import CRIS
+from .unetpp import UNetPP
+from .unet import UNet
+from .deeplabv3plus import DeepLabV3Plus
